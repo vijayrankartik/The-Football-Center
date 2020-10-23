01c8aa1dada63cbeffbf3c2e385c57683c9ff554
vijayrankartik <58977367+vijayrankartik@users.noreply.github.com>
2020-10-23 12:20:44 +0530

    Add files via upload

---
+++ components/laliga.js
@@ -0,0 +1,29 @@
+const app = document.getElementById("root")
+
+var request = new XMLHttpRequest()
+request.open('GET','https://www.scorebat.com/video-api/v1/')
+
+request.onload = function(){
+
+    var data = JSON.parse(this.response)
+    
+    if(request.status>=200 && request.status<400){
+        data.forEach(game => {
+            if(game.competition.name=="SPAIN: La Liga"){
+                
+                const div = document.createElement('div')
+                div.innerHTML = game.videos[0].embed
+
+                const a =document.createElement('a')
+                a.href = game.thumbnail
+                a.innerHTML = "here"    
+            
+                app.appendChild(div)
+                
+                console.log(game.title)
+            }        
+        });
+    }
+}
+
+request.send()
---
+++ components/sa.js
@@ -0,0 +1,28 @@
+const app = document.getElementById("root")
+
+var request = new XMLHttpRequest()
+request.open('GET','https://www.scorebat.com/video-api/v1/')
+
+request.onload = function(){
+
+    var data = JSON.parse(this.response)
+    
+    if(request.status>=200 && request.status<400){
+        data.forEach(game => {
+            if(game.competition.name=="ITALY: Serie A"){
+                
+                const div = document.createElement('div')
+                div.innerHTML = game.videos[0].embed
+
+                const a =document.createElement('a')
+                a.href = game.thumbnail
+                a.innerHTML = "here"    
+            
+                app.appendChild(div)
+            }
+            console.log(game.competition.name)        
+        });
+    }
+}
+
+request.send()
