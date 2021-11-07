8f4db6324d443fda15df770f77bccdfa74cfdc68
vijayrankartik <58977367+vijayrankartik@users.noreply.github.com>
2021-11-07 12:36:20 +0530

    futdraft added

---
+++ fut.js
@@ -0,0 +1,254 @@
+document.getElementById("gkc").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    {
+        gkfin(c);
+    }
+});
+function gkfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        // console.log(i)
+        if(i == 1)
+        document.getElementById("gkc").src = "./renders/gk1.png"
+        if(i == 2)
+        document.getElementById("gkc").src = "./renders/gk2.png"
+        if(i == 3)
+        document.getElementById("gkc").src = "./renders/gk3.png"
+        if(i == 4)
+        document.getElementById("gkc").src = "./renders/gk4.png"
+        if(i == 5)
+        document.getElementById("gkc").src = "./renders/gk5.png"
+    },200*c)
+}
+
+
+document.getElementById("lbc").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    lbfin(c);
+});
+function lbfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("lbc").src = "./renders/lb1.png"
+        if(i == 2)
+        document.getElementById("lbc").src = "./renders/lb2.png"
+        if(i == 3)
+        document.getElementById("lbc").src = "./renders/lb3.png"
+        if(i == 4)
+        document.getElementById("lbc").src = "./renders/lb4.png"
+        if(i == 5)
+        document.getElementById("lbc").src = "./renders/lb5.png"
+    },200*c)
+}
+
+
+document.getElementById("rbc").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    rbfin(c);
+});
+function rbfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("rbc").src = "./renders/rb1.png"
+        if(i == 2)
+        document.getElementById("rbc").src = "./renders/rb2.png"
+        if(i == 3)
+        document.getElementById("rbc").src = "./renders/rb3.png"
+        if(i == 4)
+        document.getElementById("rbc").src = "./renders/rb4.png"
+        if(i == 5)
+        document.getElementById("rbc").src = "./renders/rb5.png"
+    },200*c)
+}
+
+
+document.getElementById("cbrc").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    cbfin(c);
+});
+function cbfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("cbrc").src = "./renders/cb1.png"
+        if(i == 2)
+        document.getElementById("cbrc").src = "./renders/cb2.png"
+        if(i == 3)
+        document.getElementById("cbrc").src = "./renders/cb3.png"
+        if(i == 4)
+        document.getElementById("cbrc").src = "./renders/cb4.png"
+        if(i == 5)
+        document.getElementById("cbrc").src = "./renders/cb5.png"
+    },200*c)
+}
+
+
+document.getElementById("cblc").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    cblfin(c);
+});
+function cblfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("cblc").src = "./renders/cb1.png"
+        if(i == 2)
+        document.getElementById("cblc").src = "./renders/cb2.png"
+        if(i == 3)
+        document.getElementById("cblc").src = "./renders/cb3.png"
+        if(i == 4)
+        document.getElementById("cblc").src = "./renders/cb4.png"
+        if(i == 5)
+        document.getElementById("cblc").src = "./renders/cb5.png"
+    },200*c)
+}
+
+
+document.getElementById("cdm").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    cdmfin(c);
+});
+function cdmfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("cdm").src = "./renders/cdm1.png"
+        if(i == 2)
+        document.getElementById("cdm").src = "./renders/cdm2.png"
+        if(i == 3)
+        document.getElementById("cdm").src = "./renders/cdm3.png"
+        if(i == 4)
+        document.getElementById("cdm").src = "./renders/cdm4.png"
+        if(i == 5)
+        document.getElementById("cdm").src = "./renders/cdm5.png"
+    },200*c)
+}
+
+
+document.getElementById("cm").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    cmfin(c);
+});
+function cmfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("cm").src = "./renders/cm1.png"
+        if(i == 2)
+        document.getElementById("cm").src = "./renders/cm2.png"
+        if(i == 3)
+        document.getElementById("cm").src = "./renders/cm3.png"
+        if(i == 4)
+        document.getElementById("cm").src = "./renders/cm4.png"
+        if(i == 5)
+        document.getElementById("cm").src = "./renders/cm5.png"
+    },200*c)
+}
+
+
+document.getElementById("cam").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    camfin(c);
+});
+function camfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("cam").src = "./renders/cam1.png"
+        if(i == 2)
+        document.getElementById("cam").src = "./renders/cam2.png"
+        if(i == 3)
+        document.getElementById("cam").src = "./renders/cam3.png"
+        if(i == 4)
+        document.getElementById("cam").src = "./renders/cam4.png"
+        if(i == 5)
+        document.getElementById("cam").src = "./renders/cam5.png"
+    },200*c)
+}
+
+
+document.getElementById("lw").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    lwfin(c);
+});
+function lwfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("lw").src = "./renders/lw1.png"
+        if(i == 2)
+        document.getElementById("lw").src = "./renders/lw2.png"
+        if(i == 3)
+        document.getElementById("lw").src = "./renders/lw3.png"
+        if(i == 4)
+        document.getElementById("lw").src = "./renders/lw4.png"
+        if(i == 5)
+        document.getElementById("lw").src = "./renders/lw5.png"
+    },200*c)
+}
+
+
+document.getElementById("rw").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    rwfin(c);
+});
+function rwfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("rw").src = "./renders/rw1.png"
+        if(i == 2)
+        document.getElementById("rw").src = "./renders/rw2.png"
+        if(i == 3)
+        document.getElementById("rw").src = "./renders/rw3.png"
+        if(i == 4)
+        document.getElementById("rw").src = "./renders/rw4.png"
+        if(i == 5)
+        document.getElementById("rw").src = "./renders/rw5.png"
+    },200*c)
+}
+
+
+document.getElementById("st").addEventListener("click", function(){
+    for(let c = 0;c<20;c++)
+    stfin(c);
+});
+function stfin(c)
+{
+    setTimeout(function()
+    {
+        var i = Math.floor(Math.random()*5)+1
+        if(i == 1)
+        document.getElementById("st").src = "./renders/st1.png"
+        if(i == 2)
+        document.getElementById("st").src = "./renders/st2.png"
+        if(i == 3)
+        document.getElementById("st").src = "./renders/st3.png"
+        if(i == 4)
+        document.getElementById("st").src = "./renders/st4.png"
+        if(i == 5)
+        document.getElementById("st").src = "./renders/st5.png"
+    },200*c)
+}
